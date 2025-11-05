513bc78f01dec636f8753821c6a4bd4b350be3ea
Nilesh Appaso Patil <nileshpatil3806@gmail.com>
2025-11-05 16:42:09 +0530

    Add S3 public access permissions

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,48 @@
 provider "aws" {
-  region = var.aws_region
-}
+     region = var.aws_region
+   }
 
-resource "aws_s3_bucket" "webapp" {
-  bucket = var.bucket_name
-}
+   resource "aws_s3_bucket" "webapp" {
+     bucket = var.bucket_name
+   }
 
-resource "aws_s3_bucket_website_configuration" "webapp" {
-  bucket = aws_s3_bucket.webapp.id
-  
-  index_document {
-    suffix = "index.html"
-  }
-}
+   resource "aws_s3_bucket_public_access_block" "webapp" {
+     bucket = aws_s3_bucket.webapp.id
+     
+     block_public_acls       = false
+     block_public_policy     = false
+     ignore_public_acls      = false
+     restrict_public_buckets = false
+   }
 
-resource "aws_s3_object" "index" {
-  bucket       = aws_s3_bucket.webapp.id
-  key          = "index.html"
-  source       = "../src/index.html"
-  content_type = "text/html"
-}
+   resource "aws_s3_bucket_policy" "webapp" {
+     bucket = aws_s3_bucket.webapp.id
+     
+     policy = jsonencode({
+       Version = "2012-10-17"
+       Statement = [
+         {
+           Effect    = "Allow"
+           Principal = "*"
+           Action    = "s3:GetObject"
+           Resource  = "${aws_s3_bucket.webapp.arn}/*"
+         }
+       ]
+     })
+   }
+
+   resource "aws_s3_bucket_website_configuration" "webapp" {
+     bucket = aws_s3_bucket.webapp.id
+     
+     index_document {
+       suffix = "index.html"
+     }
+   }
+
+   resource "aws_s3_object" "index" {
+     bucket       = aws_s3_bucket.webapp.id
+     key          = "index.html"
+     source       = "../src/index.html"
+     content_type = "text/html"
+   }
 
